b1cbf01272b8a0fc91c67300b6a64ee18af7a110
Gabriel da Silva Zech <gabrielszech@gmail.com>
2021-11-17 21:56:22 +0100

    expand excel file

--- src/classes.py
+++ src/classes.py
@@ -1,16 +1,27 @@
 class Student:
      def __init__(self, name, id, email):
-                       self._name = name
-                       self._id = id
-                       self._email = email
+                       self.__name = name
+                       self.__id = id
+                       self.__email = email
 
 ## create getters for Student name, id and email
 
 class Room:
 
-    def __init__(self, name, capacity, quiet, tv, projector):
-        self.__name = name
+    def __init__(self, capacity, quiet, tv, projector):
         self.__capacity = capacity
         self.__quiet = quiet 
         self.__tv = tv
         self.__projector = projector
+    
+    
+    # function to get value of __capacity
+    @property
+    def capacity(self):
+        return self.__capacity
+      
+    # function to set value of __capacity
+    @capacity.setter
+    def capacity(self, c):
+        self.__capacity = c
+    
--- src/script.py
+++ src/script.py
@@ -1,29 +1,48 @@
 import functions as fn
 import classes as cl
+import pandas as pd
+import openpyxl
 
 # %%
 
 
 # create rooms
-room1=Room("2.31", 2, True, True, False)
-room2=Room("2.32", 3, True, False, False)
-room3=Room("2.35", 8, False, False, False)
-room4=Room("2.40", 2, True, True, False)
-room5=Room("3.60", 4, True, False, True)
-room6=Room("2.71", 4, False, False, False)
-room7=Room("3.01", 35, False, True, False)
-room8=Room("3.10", 7, False, True, False)
-room9=Room("3.22", 3, True, False, False)
-room10=Room("3.33", 5, True, False, False)
-room11=Room("3.35", 12, True, False, True)
-room12=Room("3.40", 15, False, True, True)
-room13=Room("3.50", 17, False, False, False)
-room14=Room("3.60", 4, True, False, False)
+# room1=Room("2.31", 2, True, True, False)
+# room2=Room("2.32", 3, True, False, False)
+# room3=Room("2.35", 8, False, False, False)
+# room4=Room("2.40", 2, True, True, False)
+# room5=Room("3.60", 4, True, False, True)
+# room6=Room("2.71", 4, False, False, False)
+# room7=Room("3.01", 35, False, True, False)
+# room8=Room("3.10", 7, False, True, False)
+# room9=Room("3.22", 3, True, False, False)
+# room10=Room("3.33", 5, True, False, False)
+# room11=Room("3.35", 12, True, False, True)
+# room12=Room("3.40", 15, False, True, True)
+# room13=Room("3.50", 17, False, False, False)
+# room14=Room("3.60", 4, True, False, False)
 
-# %%
 
 
 #%%
 
 new_student = cl.Student(fn.askStudentName(), fn.askStudentID(), fn.askStudentEmail())
+
+
+#%%
+
+df = pd.read_excel("../data/raw/rooms.xlsx")
+
+# given user input, create object with all matching available rooms
+
+des_capacity = 1
+des_quiet = True
+des_tv = False
+des_projector = False
+
+desired_room = cl.Room(1, True, False, False)
+
+print(desired_room.capacity)
+
+# after confirmation update dataframe with informations from user
 
