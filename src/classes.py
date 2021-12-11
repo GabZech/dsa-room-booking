4f1f8ef422cf48151edb1eda8f16b5f194acc71e
Max B. Eckert <mbe245@nyu.edu>
2021-12-11 23:13:45 +0100

    Update classes.py

--- src/classes.py
+++ src/classes.py
@@ -1,13 +1,16 @@
 class Student:
 
-    """
-    name (str): The name of the Student
-    id (int): The ID of the student
-    email (str): The email of the student
+    '''
+    The class Student is used to store information about student's booking preferences.
+    
+    Arributes:
+        name (str): The name of the Student
+        id (int): The ID of the student
+        email (str): The email of the student
     
     Returns:
         Student: A student object
-    """
+    '''
 
     def __init__(self, name, id, email):
                        self.__name = name
@@ -63,6 +66,9 @@ class bookingInfo:
         quiet (bool): Whether the booking is for quiet room.
         tv (bool): Whether the booking has TV.
         projector (bool): Whether the booking has projector.
+        
+     Returns:
+         Student: A student object
     '''
     
     def __init__(self, places, datetime, quiet, tv, projector):
